178c059012e582d1b7ae3460fabab51e76264ab4
nuviobco <129550046+nuviobco@users.noreply.github.com>
2023-04-08 22:48:30 -0500

    Update api key
    openai

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ tokens = nltk.word_tokenize(texto)
 
 print(tokens)
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
